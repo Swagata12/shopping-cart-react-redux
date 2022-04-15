521710150f304ea8d347f37af49be7348dde2543
Swagata12 <kunal712701@gmail.com>
2022-04-15 15:32:27 +0530

    testComit'

--- src/lib/cartLib.js
+++ src/lib/cartLib.js
@@ -4,7 +4,7 @@ export const countCart = cart => {
   const cartItemCount = cart.map(item => item.quantity).reduce((total, num) => (total + num), 0);
   const cartTotal = cart.map(item => item.Price * item.quantity).reduce((total, num) => (total + num), 0);
   return { cartItemCount, cartTotal }
-}
+}  
 
 export const highLightCartButton = () => {
   $("html, body").animate({ scrollTop: 0 }, 300);
